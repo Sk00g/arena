f3de7c409f9c0915dcc5e1db85e1ec5078c11e95
Scott Kemperman <skemperman@ascendentgroup.com>
2019-10-03 20:17:53 -0600

    fixed silly bug

--- suie/__init__.py
+++ suie/__init__.py
@@ -8,5 +8,3 @@ from suie.border import Border
 from suie.text_button import TextButton
 from suie.image_button import ImageButton
 from suie.progress_bar import *
-from suie.avatar_icon import AvatarIcon
-from suie.action_icon import ActionIcon
